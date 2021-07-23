02261df784e5501953b19da42ed2b24a9f0559d8
Egor <tegor8201@gmail.com>
2021-07-23 22:02:10 +0300

    Первая версия

--- lib/main.dart
+++ lib/main.dart
@@ -17,11 +17,58 @@ class Myfirstproject extends StatefulWidget {
 }
 
 class _MyfirstprojectState extends State<Myfirstproject> {
+  Widget buildNumberButton(int value) {
+    return ElevatedButton(
+        onPressed: () {
+          TestNum(value);
+        },
+        child: Text("$value"));
+  }
+
+  var actions = {1: "+", 2: "-"};
+  Widget buildActionButton(int action) {
+    return ElevatedButton(
+        onPressed: () {
+          action = action;
+        },
+        child: Text("${actions[action]}"));
+  }
+
+  int result = 0;
+  int action = 0;
+  int FirstNum = 0;
+  int SecondNum = 0;
+  void Final() {
+    setState(() {
+      switch (action) {
+        case 1:
+          result = FirstNum + SecondNum;
+          break;
+        case 2:
+          result = FirstNum - SecondNum;
+          break;
+        case 3:
+          result = FirstNum * SecondNum;
+          break;
+        case 4:
+          result = (FirstNum / SecondNum) as int;
+          break;
+      }
+    });
+  }
+
+  void TestNum(int Num) {
+    setState(() {
+      if (action == 0) {
+        FirstNum += FirstNum * 10 + Num;
+      } else {
+        SecondNum += SecondNum * 10 + Num;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    int result = 0;
-    int a = 0;
-    int b = 0;
     return Scaffold(
       backgroundColor: Colors.lightGreen[200],
       appBar: AppBar(
@@ -51,149 +98,69 @@ class _MyfirstprojectState extends State<Myfirstproject> {
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
+                  buildNumberButton(1),
+                  buildNumberButton(2),
+                  buildNumberButton(3),
                   Padding(
                       padding: EdgeInsets.only(
-                    top: 10,
                     left: 10,
-                    right: 10,
-                    bottom: 30,
                   )),
-                  ElevatedButton(onPressed: () {}, child: Text("1")),
-                  Padding(
-                      padding: EdgeInsets.only(
-                    top: 10,
-                    left: 10,
-                    right: 10,
-                    bottom: 30,
-                  )),
-                  ElevatedButton(onPressed: () {}, child: Text("2")),
-                  Padding(
-                      padding: EdgeInsets.only(
-                    top: 10,
-                    left: 10,
-                    right: 10,
-                    bottom: 30,
-                  )),
-                  ElevatedButton(onPressed: () {}, child: Text("3")),
-                  Padding(
-                      padding: EdgeInsets.only(
-                    top: 10,
-                    left: 10,
-                    right: 10,
-                    bottom: 30,
-                  )),
-                  ElevatedButton(onPressed: () {}, child: Text("+")),
+                  ElevatedButton(
+                      onPressed: () {
+                        action = 1;
+                      },
+                      child: Text("+")),
                 ],
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
+                  buildNumberButton(4),
+                  buildNumberButton(5),
+                  buildNumberButton(6),
                   Padding(
                       padding: EdgeInsets.only(
-                    top: 10,
                     left: 10,
-                    right: 10,
-                    bottom: 30,
                   )),
-                  ElevatedButton(onPressed: () {}, child: Text("4")),
-                  Padding(
-                      padding: EdgeInsets.only(
-                    top: 10,
-                    left: 10,
-                    right: 10,
-                    bottom: 30,
-                  )),
-                  ElevatedButton(onPressed: () {}, child: Text("5")),
-                  Padding(
-                      padding: EdgeInsets.only(
-                    top: 10,
-                    left: 10,
-                    right: 10,
-                    bottom: 30,
-                  )),
-                  ElevatedButton(onPressed: () {}, child: Text("6")),
-                  Padding(
-                      padding: EdgeInsets.only(
-                    top: 10,
-                    left: 10,
-                    right: 10,
-                    bottom: 30,
-                  )),
-                  ElevatedButton(onPressed: () {}, child: Text("-")),
+                  ElevatedButton(
+                      onPressed: () {
+                        action = 2;
+                      },
+                      child: Text("-")),
                 ],
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
+                  buildNumberButton(7),
+                  buildNumberButton(8),
+                  buildNumberButton(9),
                   Padding(
                       padding: EdgeInsets.only(
-                    top: 10,
                     left: 10,
-                    right: 10,
-                    bottom: 30,
                   )),
-                  ElevatedButton(onPressed: () {}, child: Text("7")),
-                  Padding(
-                      padding: EdgeInsets.only(
-                    top: 10,
-                    left: 10,
-                    right: 10,
-                    bottom: 10,
-                  )),
-                  ElevatedButton(onPressed: () {}, child: Text("8")),
-                  Padding(
-                      padding: EdgeInsets.only(
-                    top: 10,
-                    left: 10,
-                    right: 10,
-                    bottom: 10,
-                  )),
-                  ElevatedButton(onPressed: () {}, child: Text("9")),
-                  Padding(
-                      padding: EdgeInsets.only(
-                    top: 10,
-                    left: 10,
-                    right: 10,
-                    bottom: 30,
-                  )),
-                  ElevatedButton(onPressed: () {}, child: Text("x")),
+                  ElevatedButton(
+                      onPressed: () {
+                        action = 3;
+                      },
+                      child: Text("x")),
                 ],
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Padding(
-                      padding: EdgeInsets.only(
-                    top: 10,
-                    left: 10,
-                    right: 10,
-                    bottom: 30,
-                  )),
-                  ElevatedButton(onPressed: () {}, child: Text("00")),
-                  Padding(
-                      padding: EdgeInsets.only(
-                    top: 30,
-                    left: 10,
-                    right: 10,
-                    bottom: 10,
-                  )),
-                  ElevatedButton(onPressed: () {}, child: Text("0")),
-                  Padding(
-                      padding: EdgeInsets.only(
-                    top: 10,
-                    left: 10,
-                    right: 10,
-                    bottom: 30,
-                  )),
+                  buildNumberButton(00),
+                  buildNumberButton(0),
                   ElevatedButton(onPressed: () {}, child: Text("/")),
                   Padding(
                       padding: EdgeInsets.only(
-                    top: 10,
                     left: 10,
-                    right: 10,
-                    bottom: 30,
                   )),
-                  ElevatedButton(onPressed: () {}, child: Text("=")),
+                  ElevatedButton(
+                      onPressed: () {
+                        Final();
+                      },
+                      child: Text("=")),
                 ],
               ),
               Spacer(),
